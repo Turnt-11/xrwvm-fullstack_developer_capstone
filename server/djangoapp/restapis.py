5dc8c5fc49010983011dd2cac9f3044826c41c6d
Kyle <kylesvault@gmail.com>
2024-05-28 14:28:56 -0400

    1/2 done mod4

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -27,7 +27,7 @@ def get_request(endpoint, **kwargs):
         return response.json()
     except:
         # If any error occurs
-        print("Network exception occurred")
+        print("Network exception occurred1")
 
 # def analyze_review_sentiments(text):
 def analyze_review_sentiments(text):
@@ -38,7 +38,7 @@ def analyze_review_sentiments(text):
         return response.json()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
-        print("Network exception occurred")
+        print("Network exception occurred2")
 
 # def post_review(data_dict):
 def post_review(data_dict):
@@ -48,4 +48,4 @@ def post_review(data_dict):
         print(response.json())
         return response.json()
     except:
-        print("Network exception occurred")
+        print("Network exception occurred3")
